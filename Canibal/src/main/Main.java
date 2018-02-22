76ce3c5f019e3c16b8efe577817cd962832bde35
carlospinheirof <carlos.pinheiro.f@gmail.com>
2018-02-22 12:41:31 -0300

    Q1 inicio da Main

--- Canibal/src/main/Main.java
+++ Canibal/src/main/Main.java
@@ -1,14 +1,19 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Estado inicial = new Estado(3, 3, 0, 0, 0, 0, null);
-		ArrayList<Estado> fronteira = new ArrayList<Estado>();
+		LinkedList<Estado> fronteira = new LinkedList<Estado>();
 		fronteira.add(inicial);
-		
+		boolean resultado = false;
+		while(!resultado){
+			fronteira.removeFirst();
+		}
 		
 	}
 
